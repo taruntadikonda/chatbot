a332e83c316238c518f6463f7a46f59a58f358ed
unknown <taditarun123@gmail.com>
2019-03-24 11:33:16 +0530

    init 2

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
+var express=require('express');
+var hbs=require('express-handlebars');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var AssistantV1 = require('watson-developer-cloud/assistant/v1');
- 
+
+var app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//engine
+app.engine('hbs',hbs({extname:'hbs',layoutsDir:__dirname+'/views'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
 var assistant = new AssistantV1({
   username: 'apikey',
-  password: '47xFDKahY1S5ZZJhL5CrxAPIbgQnf1VHjdjBHQu4omX9',
-  url: 'https://gateway.watsonplatform.net/assistant/api',
+  password: 'AoaluWv-PXNaETwjbalpUXTuuz2O0SHYveuKAxXy1Eso',
+  url: 'https://gateway-lon.watsonplatform.net/assistant/api',
   version: '2019-03-23'
 });
  
-assistant.message(
-  {
-    input: { text: "show me resturats" },
-    workspace_id: '3d776848-262a-4872-b745-1c671da28ebf'
-  },
-  function(err, response) {
-    if (err) {
-      console.error(err);
-    } else {
-      //console.log(JSON.stringify(response, null, 2));
-      console.log(response.output.text);
+app.get('/',function(req,res){
+  //console.log(req.query.valid);
+  res.render('error');
+})
+
+app.post('/',function(req,res){
+  console.log(req.body.entry);
+  assistant.message(
+    {
+      input: { text: req.body.entry},
+      workspace_id: '2390703a-70de-4939-9284-aa9a63f10161'
+    },
+    function(err, response) {
+      if (err) {
+        console.error(err);
+      } else {
+        //console.log(JSON.stringify(response, null, 2));
+        console.log(response.output.text);
+        res.redirect("/");
+      }
     }
-  }
-);
+  );
+  
+  
+})
+
+
+
+app.listen(8000,function(){
+  console.log("server at port 8000");
+})
